d36e8adaf71558912400b43773990224b4255c9c
Fucloud <270928474@qq.com>
2023-09-26 17:49:52 +0800

    perf: 优化插件激活逻辑

--- src/extension.ts
+++ src/extension.ts
@@ -1,51 +1,58 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Uri } from 'vscode'; 
+
 import {handle} from './handler';
 import {initConfig} from './config';
+import { checkFile, getRootUri } from './fsUtils';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+const CARGO_TOML = "Cargo.toml";
+
+let rsWatcher: vscode.FileSystemWatcher;
+let rootUri: Uri;
+
+export async function activate(context: vscode.ExtensionContext) {
+	// 初始化配置信息
 	initConfig();
 
-	// 查找根目录
-	let rootUri: vscode.Uri;
-	let entries = vscode.workspace.workspaceFolders;
-	if (entries !== undefined) {
-		rootUri = entries[0].uri;
+	// 初始化 rootUri
+	let tmpRootUri = await getRootUri();
+	if(tmpRootUri === undefined) {
+		console.log("查找工作路径失败!");
+		return;
+	} else {
+		rootUri = tmpRootUri;
+	}
+
+	let cargoTomlUri = vscode.Uri.joinPath(rootUri, CARGO_TOML);
+	const tomlWatcher = vscode.workspace.createFileSystemWatcher(cargoTomlUri.fsPath);
+	// Cargo.toml删除 插件重新激活
+	tomlWatcher.onDidDelete(async (_) => {
+		reactivate();
+		console.log("Cargo.toml删除，重新激活!");
+	});
+	// Cargo.toml创建 插件不激活
+	tomlWatcher.onDidCreate(async (_) => {
+		deactivate();
+		console.log("Cargo.toml创建，不激活!");
+	});
+	// 根目录中存在Cargo.Toml 不激活函数
+	if(await checkFile(cargoTomlUri)) {
+		console.log("存在Cargo.toml项目，不激活!");
+		return;
 	}
 	
+	reactivate();
+}
+
+function reactivate() {
     // 当创建文件的时候激活
-    const watcher = vscode.workspace.createFileSystemWatcher("**/*.rs");
-    watcher.onDidCreate(async (fileUri: vscode.Uri) => {
+	rsWatcher = vscode.workspace.createFileSystemWatcher("**/*.rs");
+    rsWatcher.onDidCreate(async (fileUri: Uri) => {
 		handle(rootUri, fileUri);
  	});
-
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "test-fs" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('rust-project.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from test-fs!');
-	});
-
-	context.subscriptions.push(disposable);
-
-
-	let disposable2 = vscode.commands.registerCommand('rust-project.addFile', async (uri: vscode.Uri) => {
-
-		vscode.window.showInformationMessage(uri. toString());
-	});
-
-	context.subscriptions.push(disposable2);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	rsWatcher.dispose();
+}
