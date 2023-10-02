8aa6d72cc7c08f3d2933d116c24d9a1dabe55e06
Fucloud-arch <270928474@qq.com>
2023-10-02 11:46:38 +0800

    feat:  扩充两个基本对象的成员函数

--- src/projectInfo.ts
+++ src/projectInfo.ts
@@ -3,19 +3,64 @@
 // https://zhuanlan.zhihu.com/p/654967992
 // https://github.com/Fucloud233/ra-exmaple/blob/method-3/.vscode/settings.json
 
-import * as vscode from 'vscode';
+import { Uri }  from 'vscode';
 import * as config from './config';
 
 // 项目配置文件名
 export const PROJECT_FILE_NAME = "rust-project.json";
 
 export class ProjectInfo {
-    "sysroot": string;
-    "crates": Crate[];
+    private "sysroot": string;
+    private "crates": Crate[];
 
-    constructor(crates: Crate[]) {
+    constructor(crates: Crate[] = []) {
         this.sysroot = config.getProjectInfo(config.SYSROOT);
         this.crates = crates;
+    }
+
+    setCrate(crate: Crate, index: number) {
+        this.crates[index] = crate;
+
+        console.log("after set:");
+        console.log(this.crates);
+    }
+
+    removeCrate(index: number) {
+        this.crates.splice(index, 1);
+    }
+
+    pushCrate(crate: Crate) {
+        this.crates.push(crate);
+    }
+
+    checkCrate(crate: Crate, isStrict: boolean=false): boolean {
+        for(let _curCrate of this.crates) {
+            let curCrate = Object.assign(new Crate(), _curCrate);
+            if(curCrate.isEqual(crate, isStrict)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    findCrate(crate: Crate, isStrict: boolean=false): number {
+        for(let i=0; i<this.crates.length; i++) {
+            let curCrate = Object.assign(new Crate(), this.crates[i]);
+            if(curCrate.isEqual(crate, isStrict)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    findCrateWithUri(fileUri: Uri): number {
+        for(let i=0; i<this.crates.length; i++) {
+            let curCrate = Object.assign(new Crate(), this.crates[i]);
+            if(curCrate.isEqualWithUri(fileUri)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
 
@@ -26,9 +71,22 @@ export class Crate {
     "edition": "2015" | "2018" | "2021";
     "deps": string[];
 
-    constructor(fileName: vscode.Uri) {
+    constructor(fileName: Uri=Uri.parse("")) {
         this.root_module = fileName.fsPath;
         this.edition = config.getProjectInfo(config.DEFAULT_EDITION);
         this.deps = [];
+    }
+
+    isEqualWithUri(fileUri: Uri): boolean {
+        return this.root_module === fileUri.fsPath;
+    }
+
+    isEqual(other: Crate, isStrict: boolean=false): boolean {
+        if(isStrict) {
+            return this.root_module === other.root_module &&
+                this.edition === other.edition;
+        } else {
+            return this.root_module === other.root_module;
+        }
     }
 }
