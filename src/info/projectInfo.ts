8b3281a77e5c10e10baf65f60304771149a7a403
Fucloud-arch <270928474@qq.com>
2023-10-04 15:25:50 +0800

    refactor: 重构Crate对象路径表示

--- src/info/projectInfo.ts
+++ src/info/projectInfo.ts
@@ -4,8 +4,10 @@
 // https://github.com/Fucloud233/ra-exmaple/blob/method-3/.vscode/settings.json
 
 import { Uri }  from 'vscode';
+
 import * as config from '../config';
 import { Exclude, Expose, Type } from 'class-transformer';
+import { getAbsoluteUri, getRelativeUri } from '../utils/fs';
 
 // 项目配置文件名
 export const PROJECT_FILE_NAME = "rust-project.json";
@@ -74,18 +76,32 @@ export class ProjectInfo {
             return elem.isEqualWithUri(fileUri);
         });
     }
+
+    /**
+     * 用于验证crates的依赖树
+     */
+    updatesCrates() {
+
+    }
 }
 
 export class Crate {
-    // TODO: 考虑使用相对路径
+    // 既保留绝对路径 又保留相对路径
     @Exclude()
-    private _root_module: Uri;
+    private _root_module: Uri ;
+    @Exclude()
+    private _relative_root_module: string;
+
+    private display_name: string;
     // [注意] 此处是string类型
     private "edition": "2015" | "2018" | "2021";
     private "deps": string[];
 
     constructor(fileName: Uri=Uri.parse("")) {
+        this._relative_root_module = getRelativeUri(fileName);
         this._root_module = fileName;
+
+        this.display_name = "";
         this.edition = config.getProjectInfo(config.DEFAULT_EDITION);
         this.deps = [];
     }
@@ -106,11 +122,12 @@ export class Crate {
 
     // 通过getter/setter重新定义字段root_module
     @Expose({name: "root_module"})
-    get rootModule():string {
-        return this._root_module.fsPath;
+    get rootModule(): string {
+        return this._relative_root_module;
     }
 
     set rootModule(uriStr: string) {
-        this._root_module = Uri.parse(uriStr);
+        this._root_module = getAbsoluteUri(uriStr);
+        this._relative_root_module = uriStr;
     }
 }
