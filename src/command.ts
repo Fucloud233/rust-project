a86f85d06ac082b4dcd6734ea91a0e06c3ba7708
Fucloud-arch <270928474@qq.com>
2023-10-05 11:01:44 +0800

    feat: 上传importCrate命令

--- src/command.ts
+++ src/command.ts
@@ -1,6 +1,8 @@
-import * as vscode from 'vscode';
-import { getRootUri } from './utils/fs';
+import { window, commands, QuickPickItem } from 'vscode';
+
 import { SettingsFile } from './info/settingsFile';
+import { Crate, Dep } from './info/projectInfo';
+import { getRelativeUri } from './utils/fs';
 
 class Command {
     name: string;
@@ -12,28 +14,57 @@ class Command {
     }
 }
 
-export const addCrateToCmd = vscode.commands.registerTextEditorCommand(
-    "rust-project.auto.addCrateTo",
+class CrateItem implements QuickPickItem {
+    label: string;
+    description:string;
+    index: number;
+    
+    constructor(crate: Crate, index: number) {
+        this.label = crate.displayName;
+        this.description = crate.relativeRootModule;
+        this.index = index;
+    }
+
+    isEqualWithUri(fileUri: string) {
+        return this.description === fileUri;
+    }
+}
+
+export const addCrateToCmd = commands.registerTextEditorCommand(
+    "rust-project.auto.importCrate",
     async (editor) => {
-        let rootUri = await getRootUri();
-        if(rootUri === undefined) {
-            return;
+        // 1. 读取项目信息
+        let settingsFile = new SettingsFile();
+        await settingsFile.load();
+        
+        let crates = settingsFile.cratesFromFirstProject;
+        if(crates.length === 0) {
+            window.showInformationMessage("There are not crates in your workspace.");
         }
 
-        // 获取可以添加的选项
-        let settingsFile = new SettingsFile(rootUri);
-        await settingsFile.load();
-        let options = settingsFile.getCratesRelativeUri(rootUri);
+        // 2. 获取当前Crate 与其Index
+        let curFileUri = getRelativeUri(editor.document.fileName);
+        let curFileIndex = crates.findIndex((crate) => crate.isEqualWithUri(curFileUri));
 
-        vscode.window.showQuickPick(options)
-            .then((selectedOption) => {
-                if(selectedOption) {
-                    options.indexOf(selectedOption);                 
-                    vscode.window.showInformationMessage(`Your select ${selectedOption}`);
+        // 3. 打开QuickPick
+        const input = window.createQuickPick<CrateItem>();
+        input.placeholder = "Select the crate you want to import.";
+        // [注意] map和filter的顺序
+        input.items = crates.map((crate, index, _array) => new CrateItem(crate, index))
+            .filter((crate, _index, _array) => !crate.isEqualWithUri(curFileUri));
+        input.show(); 
+        input.onDidAccept(async () => {
+            // 获取选择Item的信息
+            let selectItem = input.selectedItems[0];
+            let index = selectItem.index,
+                displayName = selectItem.label;
+            // 添加并保存依赖
+            // console.log(curFileIndex, displayName);
+            crates[curFileIndex].appendDep(new Dep(index, displayName));
+            await settingsFile.save();
 
-                    //TODO: 继续完成选择Crate的操作
-                }
-            });
+            input.dispose();
+        });
     }
 );
 
