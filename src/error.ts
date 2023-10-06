a83a0732296560cf5a5dbbc1d0450b902d1f3f5c
Fucloud-arch <270928474@qq.com>
2023-10-06 16:49:46 +0800

    feat: 添加错误类型

--- src/error.ts
+++ src/error.ts
@@ -1,3 +1,5 @@
+export const NOT_KNOW_ERROR = "Not know error!";
+
 export class BaseError extends Error {
     message: string;
 
@@ -22,5 +24,28 @@ export class NotFoundError extends BaseError {
 export class FileParseError extends BaseError {
     constructor(fileName: string) {
         super(fileName + ' parsing falied. ');
+    }
+}
+
+export class DepNotFoundError extends NotFoundError {
+    crateName: string;
+    depName: string;
+
+    constructor(crateName: string, depName: string) {
+        super(`The "${depName} in "${crateName}"`);
+        this.crateName = crateName;
+        this.depName = depName;
+    }
+}
+
+export class CrateNotFoundError extends NotFoundError {
+    constructor(crateName: string) {
+        super(`The crate "${crateName}"`);
+    }
+}
+
+export class ImportCyclesError extends BaseError {
+    constructor(crates: string[]) {
+        super("Import Cycles: " + crates.join("->"));
     }
 }
