41384a782e2563ebbc5dcd35d259efe3fb493739
Fucloud-arch <270928474@qq.com>
2023-10-29 12:05:16 +0800

    feat: 实现创建/验证存在ProjectFile的函数

--- src/info/projectFile.ts
+++ src/info/projectFile.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import { Uri } from 'vscode';
-import { checkFile, writeJsonFile, readJsonFile } from '../utils/fs';
+import { getRootUri, checkFileExist, writeJsonFile, readJsonFile } from '../utils/fs';
 import { ProjectInfo } from './projectInfo';
 import Crate from './Crate';
 
@@ -16,13 +16,17 @@ export class ProjectFile {
     }
 
     // 初始化配置信息文件
-    init(crate: Crate) {
-        this.fileJson = new ProjectInfo([crate]);
+    init(crate: Crate | undefined) {
+        if(crate === undefined) {
+            this.fileJson = new ProjectInfo([]);
+        } else {
+            this.fileJson = new ProjectInfo([crate]);
+        }
     }
 
     async load() {
         try {
-            if(await checkFile(this.fileUri)) {
+            if(await checkFileExist(this.fileUri)) {
                 this.fileJson = await readJsonFile(this.fileUri);
             }
         } catch (error) {
@@ -43,6 +47,12 @@ export class ProjectFile {
     }
 }
 
+// 判断ProjectFile是否存在
+export async function checkProjectFileExist(folderPath: Uri): Promise<boolean> {
+    let projectFileUri = Uri.joinPath(folderPath, PROJECT_FILE_NAME);
+    return await checkFileExist(projectFileUri) !== undefined;
+}
+
 // 返回值 [Uri, boolean], 前者是Project文件路径，后者是是否存在
 // 如果文件不存在则会默认返回文件当前路径下的
 // TODO: 从文件的当前目录往根目录查找
@@ -58,13 +68,12 @@ export async function getProjectFileUri(rootPath: Uri, filePath: Uri):
     // 判断是否存在项目文件
     let curPath = rootPath;
     for (let subPath of subPaths) {
-        // 获取项目文件的Uri
-        let projectFileUri = Uri.joinPath(curPath, PROJECT_FILE_NAME);
-
         // console.log("projectFileUri: ", projectFileUri);
         
         // 验证文件是否存在
-        if(await checkFile(projectFileUri)) {
+        if (await checkProjectFileExist(curPath)) {
+            // 获取项目文件的Uri
+            let projectFileUri = Uri.joinPath(curPath, PROJECT_FILE_NAME);
             return [projectFileUri, true];
         } else {
             curPath = Uri.joinPath(curPath, subPath);
@@ -72,4 +81,32 @@ export async function getProjectFileUri(rootPath: Uri, filePath: Uri):
     }
 
     return [curPath, false];
+}
+
+// 判断文件夹上级是否存在当前文件夹
+export async function checkProjectFileExistInParentDir(folderPath: Uri): 
+        Promise<[boolean, Uri | undefined]>{
+    // let parentDir = getRelativeUri(folderPath);
+    let parentDir = Uri.joinPath(folderPath, PROJECT_FILE_NAME).fsPath;
+
+    while(parentDir !== getRootUri().fsPath) {
+        parentDir = path.dirname(parentDir);
+
+        let parentDirUri = Uri.parse(parentDir);
+        if(await checkProjectFileExist(parentDirUri)) {
+            return [true, parentDirUri];
+        }
+    }
+
+    return [false, undefined];
+} 
+
+// 生成项目文件路径
+export function createProjectFile(folderPath: Uri): Uri {   
+    let projectFileUri = Uri.joinPath(folderPath, PROJECT_FILE_NAME);
+    let projectFile = new ProjectFile(projectFileUri);
+    projectFile.init(undefined);
+    projectFile.save();
+
+    return projectFileUri;
 }
