09e9eae1b031715cba4e450338b60b7a40c97319
Fucloud <270928474@qq.com>
2023-10-31 10:03:52 +0800

    refactor: merge the auto and manual mode

--- src/config.ts
+++ src/config.ts
@@ -8,18 +8,6 @@ export const EXTENSION_NAME = "rust-project";
 // 配置信息字段名
 const PROJECT_INFO = "projectInfo";
 
-/**
- * @deprecated 已经迁移为Create Method
- */
-export enum SaveMethod {
-    settings = 1,
-    rustProject
-}
-
-export enum CreateMethod {
-    auto, manual
-}
-
 export enum PathType {
     relative, absolute
 }
@@ -28,21 +16,7 @@ class ProjectConfigInfo {
     sysroot: string = "";
     defaultEdition: "2015" | "2018" | "2021" = "2015";
     @Exclude()
-    private _createMethod: CreateMethod = CreateMethod.auto;
-    @Exclude()
     private _pathType: PathType = PathType.relative;    
-    @Exclude()
-    private _saveMethod: SaveMethod = SaveMethod.rustProject;
-
-    @Expose({name: "createMethod"})
-    @IsEnum(CreateMethod)
-    get createMethod(): CreateMethod {
-        return this._createMethod;
-    }
-    set createMethod(methodStr: string) {
-        let methodkey = methodStr as keyof typeof CreateMethod;
-        this._createMethod = CreateMethod[methodkey];
-    }
 
     @Expose({name: "pathType"})
     @IsEnum(PathType)
@@ -52,16 +26,6 @@ class ProjectConfigInfo {
     set pathType(typeStr: string) {
         let typeKey = typeStr as keyof typeof PathType;
         this._pathType = PathType[typeKey];
-    }
-
-    @Expose({name: "saveMethod"})
-    @IsEnum(SaveMethod)
-    get saveMethod(): SaveMethod {
-        return this._saveMethod;
-    }
-    set saveMethod(methodStr: string) {
-        let methodkey = methodStr as keyof typeof SaveMethod;
-        this._saveMethod = SaveMethod[methodkey];
     }
 }
 
@@ -124,14 +88,9 @@ export function getConfig(section: string): any {
     return vscode.workspace.getConfiguration(section);
 }
 
-/* ------------------------------ 废弃代码 ------------------------------ */
-
 export const SYSROOT = "sysroot";
 export const DEFAULT_EDITION = "defaultEdition";
-/**
- * @deprecated 已经迁移为Create Method
- */
-export const SAVE_METHOD = "saveMethod";
+
 // 创建Project的方式
 export const CREATE_METHOD = "createMethod";
 
@@ -142,29 +101,5 @@ export function getProjectInfo(section: string = ""): any {
         return projectInfo;
     } else {
         return projectInfo.get(section);
-    }
-}
-
-/**
- * @deprecated 已经迁移为Create Method
- */
-// 获取配置信息
-export function getSaveMethod(): SaveMethod {
-    const saveMethod = getProjectInfo(SAVE_METHOD);
-
-    switch(saveMethod) {
-        case "settings": return SaveMethod.settings;
-        case "rust-project": return SaveMethod.rustProject;
-        default: return SaveMethod.settings;
-    }
-}
-
-export function getCreateMethod(): CreateMethod {
-    const createMethod = getProjectInfo(CREATE_METHOD);
-
-    switch(createMethod) {
-        case "auto": return CreateMethod.auto;
-        case "manual": return CreateMethod.manual;
-        default: return CreateMethod.auto; 
     }
 }
--- src/handler.ts
+++ src/handler.ts
@@ -1,33 +1,10 @@
 import { Uri, window } from 'vscode';
 
-import { projectConfig, CreateMethod } from './config';
 import { getSettingsFile } from './info/settingsFile';
-import { BaseError, ExistError } from './error';
-import Crate from './info/Crate';
 import { ProjectFile } from './info/projectFile';
+import Crate from './info/Crate';
 
-// 向settingsFile中添加Crate
-// [注意] 添加Crate不会影响crates的顺序
-async function appendCrateToSettingsFile(fileUri: Uri) {
-    let settingsFile = getSettingsFile();
-
-    try {
-        settingsFile.appendCrateToProjectInfo(new Crate(fileUri));
-        // console.log("SettingsFile: ", settingsFile.fileInfo.firstProject);
-        // [注意] 保存不能写在finally 当出现错误时 可能会空白覆盖
-        await settingsFile.save();
-    } catch(err) {
-        if(err instanceof ExistError) {
-            window.showWarningMessage(err.message + " And it will be covered.");
-            await settingsFile.save();
-        } else if(err instanceof BaseError) {
-            window.showErrorMessage(err.message);
-        } else {
-            console.log("Not known error: ", err);
-            window.showErrorMessage("Not known error!");
-        }
-    }
-}
+import { BaseError, ExistError } from './error';
 
 export async function handleCreate(fileUri: Uri) {
     let settingsFile = getSettingsFile();
@@ -37,22 +14,22 @@ export async function handleCreate(fileUri: Uri) {
     
     try {
         // the rust-project exists
-        if(longestPath !== undefined) {
-            let crate = new Crate(fileUri, longestPath);
-
-            let projectFile = new ProjectFile(
-                ProjectFile.toProjectFileUri(longestPath)
-            );
-            await projectFile.load();
-            projectFile.pushCrate(crate);
-            projectFile.save();
+        if(longestPath === undefined) {
+            let crate = new Crate(fileUri);
+            settingsFile.appendCrateToProjectInfo(crate);
+            settingsFile.save();
             return;
-        }
+        } 
 
-        let crate = new Crate(fileUri);
-        settingsFile.appendCrateToProjectInfo(crate);
-        // [注意] 保存不能写在finally 当出现错误时 可能会空白覆盖
-        settingsFile.save();
+        let crate = new Crate(fileUri, longestPath);
+
+        let projectFile = new ProjectFile(
+            ProjectFile.toProjectFileUri(longestPath)
+        );
+        await projectFile.load();
+        projectFile.pushCrate(crate);
+        projectFile.save();
+        return;
     } catch(err) {
         if(err instanceof ExistError) {
             window.showWarningMessage(err.message + " And it will be covered.");
@@ -67,17 +44,7 @@ export async function handleCreate(fileUri: Uri) {
 }
 
 export async function handleDelete(fileUri: Uri) {
-    switch(projectConfig.createMethod) {
-        // 自动存储在settings中
-        case CreateMethod.auto: {
-            let settingsFile = getSettingsFile();
-            settingsFile.removeCrateFromProjectInfo(fileUri);
-            await settingsFile.save();
-            break;
-        };
-        // 启用手动存储模式
-        case CreateMethod.manual: {
-
-        }
-    }
+    let settingsFile = getSettingsFile();
+    settingsFile.removeCrateFromProjectInfo(fileUri);
+    await settingsFile.save();
 }
