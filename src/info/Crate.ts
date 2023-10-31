1fca462884385613ef1b70c40eec49c58395fbef
Fucloud <270928474@qq.com>
2023-10-31 18:09:51 +0800

    refactor: define the difference with _/root_module feat: implement updateRootModule

--- src/info/Crate.ts
+++ src/info/Crate.ts
@@ -1,19 +1,27 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { Exclude, Expose, Type } from "class-transformer";
 import { Uri } from "vscode";
-import path = require("path");
 
-import { getAbsoluteUri, getRelativeUri, uriToString } from "../utils/fs";
+import { getRelativeUri, getRootUri, toAbsoluteUri } from "../utils/fs";
 import { relativeUriToCrateName } from "../utils/info";
-import { PathType, projectConfig } from "../config";
+import { projectConfig } from "../config";
 import Dep from "./Dep";
 
 export default class Crate {
-    // 既保留绝对路径 又保留相对路径
+    /* the difference between _root_module and root_module
+        * _root_module
+            - type: vscode.URI
+            - is absolute path
+            - for file reading and writing
+            - for comparison between Cates
+        * root_module
+            - type: string
+            - is relative path
+            - is the value of field 'root_module'
+     */
     @Exclude()
     private _root_module: Uri ;
-    @Exclude()
-    private _relative_root_module: string;
+    private root_module: string;
 
     private display_name: string;
     // [注意] 此处是string类型
@@ -21,23 +29,19 @@ export default class Crate {
     @Exclude()
     private _deps: Dep[];
 
-    constructor(fileName: Uri=Uri.parse(""), folderPath: Uri | undefined=undefined) {
-        this._relative_root_module = getRelativeUri(fileName, folderPath);
+    constructor(fileName: Uri=Uri.parse(""), folderUri: Uri=getRootUri()) {
+        // the folderPath is critical to get absolute path 
+        this.root_module = getRelativeUri(fileName, folderUri);
         this._root_module = fileName;
 
-        this.display_name = relativeUriToCrateName(this._relative_root_module);
+        this.display_name = relativeUriToCrateName(this.root_module);
         this.edition = projectConfig.defaultEdition;
         this._deps = [];
     }
 
-    isEqualWithUri(fileUri: Uri | string): boolean {
-        // [注意] 不能直接比较Uri对象 需要比较fsPath对象
-        let fileUriStr = uriToString(fileUri);
-        if(path.isAbsolute(fileUriStr)) {
-            return fileUriStr === this._root_module.fsPath;
-        } else {
-            return fileUriStr === this._relative_root_module;
-        }
+    isEqualWithUri(filePath: Uri): boolean {
+        // Important: you should compare with fsPath instead of URI Object
+        return filePath.fsPath === this._root_module.fsPath;
     }
 
     isEqualWithName(name: string): boolean {
@@ -79,35 +83,22 @@ export default class Crate {
         return this.deps.length > 0;
     }
 
-    // 通过getter/setter重新定义字段root_module
-    @Expose({name: "root_module"})
-    get rootModule(): string {
-        switch(projectConfig.pathType) {
-            case PathType.relative: return this._relative_root_module;
-            case PathType.absolute: return this._root_module.fsPath;
-        }
+    // update folderPath for correcting the root_module
+    // the crate using root_folder as the folderPath
+    updateRootModule(folderPath: Uri) {
+        this._root_module = toAbsoluteUri(this.relativeRootModule, folderPath);
     }
 
-    set rootModule(uriStr: string) {
-        if(path.isAbsolute(uriStr)) {
-            this._root_module = Uri.parse(uriStr);
-            this._relative_root_module = getRelativeUri(this._root_module);
-        } else {
-            this._root_module = getAbsoluteUri(uriStr);
-            this._relative_root_module = uriStr;
-        }
+    get rootModule(): Uri {
+        return this._root_module;
+    }
+
+    get relativeRootModule(): string {
+        return this.root_module;
     }
 
     get displayName(): string {
         return this.display_name;
-    }
-
-    get relativeRootModule(): string {
-        return this._relative_root_module;
-    }
-
-    get absoluteRootModule(): string {
-        return this._root_module.fsPath;
     }
 
     @Type(() => Dep)
