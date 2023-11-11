9ddd600144fb846310a7d82d1524c954bf14a821
fucloud-win11 <270928474@qq.com>
2023-11-11 23:32:03 +0800

    fix: display error info

--- src/handler.ts
+++ src/handler.ts
@@ -4,14 +4,14 @@ import { getSettingsFile } from './info/settingsFile';
 import { ProjectFile } from './info/projectFile';
 import Crate from './info/Crate';
 
-import { BaseError, ExistError } from './error';
+import { handleError, ExistError } from './error';
 
 export async function handleCreate(fileUri: Uri) {
-    let settingsFile = getSettingsFile();
-    // longest path matching
-    let longestPath = settingsFile.getLongestMatchingPath(fileUri);
-    
     try {
+        let settingsFile = getSettingsFile();
+        // longest path matching
+        let longestPath = settingsFile.getLongestMatchingPath(fileUri);
+
         // the rust-project exists
         if(longestPath === undefined) {
             let crate = new Crate(fileUri);
@@ -26,33 +26,35 @@ export async function handleCreate(fileUri: Uri) {
         await projectFile.load();
         projectFile.pushCrate(crate);
         projectFile.save();
-        return;
-    } catch(err) {
-        if(err instanceof ExistError) {
-            window.showWarningMessage(err.message + " And it will be covered.");
-            await settingsFile.save();
-        } else if(err instanceof BaseError) {
-            window.showErrorMessage(err.message);
-        } else {
-            console.log("Not known error: ", err);
-            window.showErrorMessage("Not known error!");
+    } catch(error) {
+        if(error instanceof ExistError) {
+            window.showWarningMessage(error.message + " And it will be covered.");
+            getSettingsFile().save();
         }
+
+        // TODO: when settingsFile is not loaded, you can cancel it
+        
+        handleError(error);
     }
 }
 
 export async function handleDelete(fileUri: Uri) {
-    let settingsFile = getSettingsFile();
-    // longest path matching
-    let longestPath = settingsFile.getLongestMatchingPath(fileUri);
-    
-    if(longestPath === undefined) {
-        settingsFile.removeCrateFromProjectInfo(fileUri);
-        await settingsFile.save();
-        return;
-    }
+    try {
+        let settingsFile = getSettingsFile();
+        // longest path matching
+        let longestPath = settingsFile.getLongestMatchingPath(fileUri);
+        
+        if(longestPath === undefined) {
+            settingsFile.removeCrateFromProjectInfo(fileUri);
+            await settingsFile.save();
+            return;
+        }
 
-    let projectFile = new ProjectFile(longestPath);
-    await projectFile.load();
-    projectFile.removeCrateWithUri(fileUri);
-    projectFile.save();
+        let projectFile = new ProjectFile(longestPath);
+        await projectFile.load();
+        projectFile.removeCrateWithUri(fileUri);
+        projectFile.save();
+    } catch(error) {
+        handleError(error);
+    }
 }
